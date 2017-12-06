9f258e620641ce28a9bf626a75c1be332e9a093f
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-06 13:54:09 -0500

    Updated from Brython Server: 12/6/2017 1:54:08 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,16 @@
+#Charlie Goodrich
+#12/06/17
+#longestDictionaryWord.py - prints the longest word in the dictionary
 
+dictionary = open("engmix.txt")
+
+longest = 0
+word = ""
+
+for words in dictionary:
+    length = len(words)
+    if length > longest:
+        longest = length
+        word = words
+        
+print("The longest word is", word, "with", longest, "characters")
