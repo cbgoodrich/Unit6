0f2f411a4fceb3ae7365ba981e6718da28d558ad
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-06 13:42:03 -0500

    Updated from Brython Server: 12/6/2017 1:42:01 PM

--- fileDemo.py
+++ fileDemo.py
@@ -2,5 +2,10 @@
 #12/06/17
 #fileDemo.py - how to read a file
 
-open("engmix.txt")
+dictionary = open("engmix.txt")
 
+wordCount = 0
+for word in dictionary:
+    wordCount += 1
+    
+print("There are", wordCount, "words in the dictionary")
