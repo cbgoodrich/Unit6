685b76a83401c82a50fd0b94f5ecfd52659ad6e6
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-06 14:55:10 -0500

    Updated from Brython Server: 12/6/2017 2:55:09 PM

--- howManyWords.py
+++ howManyWords.py
@@ -4,5 +4,13 @@
 
 dictionary = open("engmix.txt")
 
-def howManyWords(count):
+def howManyWords(count): #where count is the number of letters in a word
+    wordCount = 0
+    for word in dictionary:
+        if len(word.strip()) == count:
+            wordCount += 1
+    
+    print("There are", wordCount, "words that have", count, "letters")
+    
+howManyWords(1)
     
