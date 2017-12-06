7654f2cd930a940af006d6d1a6b68443b8a36577
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-06 13:57:29 -0500

    Updated from Brython Server: 12/6/2017 1:57:28 PM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,9 @@
+#Charlie Goodrich
+#12/06/17
+#zzwords.py - prints all the words from the dictionary that have 'zz' in them
 
+dictionary = open("engmix.txt")
+
+for word in dictionary:
+    if "zz" in word:
+        print(word.strip())
