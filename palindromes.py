d899588a98d12e0af75e6985da74e9417374af47
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-07 14:31:49 -0500

    Updated from Brython Server: 12/7/2017 2:31:48 PM

--- palindromes.py
+++ palindromes.py
@@ -1,1 +1,13 @@
+#Charlie Goodrich
+#12/07/17
+#palindromes.py - finds the palindromes in a dictionary
+
+dictionary = open("engmix.txt")
+
+for word in dictionary:
+    word = word.strip()
+    reverse = list(word).reverse()
+    if list(word) == reverse:
+        print(word)
+
 
