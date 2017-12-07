7493e748215c1d5f9c43bc1030d44d5a1bac4467
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-07 14:18:30 -0500

    Updated from Brython Server: 12/7/2017 2:18:22 PM

--- reverseFile.py
+++ reverseFile.py
@@ -2,7 +2,14 @@
 #12/06/17
 #reverseFile.py - asks the user for a file, reverses the lines in the file
 
-file = open(""+input("Enter your file: ")+"")
+file = open(""+input("Enter a file: ")+"")
 
-for word in file:
-        
+fileList = []
+
+for line in file:
+    fileList.append(line)
+    
+fileList.reverse()
+
+for line in fileList:
+    print(line.strip())
