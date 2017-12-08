f7c6e07e98f2643e6293942d154576ded27acf7d
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-08 14:38:56 -0500

    Updated from Brython Server: 12/8/2017 2:38:55 PM

--- homework.py
+++ homework.py
@@ -1,1 +1,33 @@
+#Charlie Goodrich
+#12/08/17
+#homework.py - the homework sheet boi
+
+dictionary = open("engmix.txt")
+
+def wordAsk():
+    word = input("Enter a word: ")
+    
+    counter = 0
+    for words in dictionary:
+        if words.strip() == word:
+            print("Your word is in the dictionary")
+            counter += 1
+            break
+    if counter == 0:
+        print("Your word is not in the dictionary")
+        
+
+def numberWord():
+    number = int(input("What number word do you want? "))
+    
+    wordList = []
+    for words in dictionary:
+        word = words.strip()
+        wordList.append(word)
+    print("The number", number, "word in the dictionary is","'"+wordList[number-1]+"'")
+    
+numberWord()
+
+
+
 
