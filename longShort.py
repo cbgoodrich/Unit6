e902eb8ee2c0e95209a5813680b89845c09af908
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-11 14:54:20 -0500

    Update longShort.py

--- longShort.py
+++ longShort.py
@@ -4,10 +4,17 @@
 
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-wordList = [""]*26
+longList = [""]*26
+shortList = [""]*26
 
 dictionary = open("engmix.txt")
 
-for words in dictionary:
-    print(alphabet[alphabet.index(words[0])])
-    break
+for word in dictionary:
+    if longList[alphabet.index(word[0])] == "":
+        longList[alphabet.index(word[0])] = word
+    else:
+        if len(word) > len(longList[alphabet.index(word[0])]):
+            longList[alphabet.index(word[0])] = word
+    else:
+        
+    
