ce0cc72c00fef7058cc5965a02ef4d82c655ecd2
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-11 14:26:34 -0500

    Updated from Brython Server: 12/11/2017 2:26:33 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#12/11/17
+#warmup16.py - finds the words in the dictionary that start with first initial, end with last initial
+
+dictionary = open("engmix.txt")
+
+first_Name = input("What's your first name? ")
+last_Name = input("What's your last initial? ")
+
+firstName = first_Name.lower()
+lastName = last_Name.lower()
+
+for words in dictionary:
+    word = words.strip()
+    if word != "":
+        if word[0] == firstName[0] and word[-1] == lastName[0]:
+            print(word)
 
